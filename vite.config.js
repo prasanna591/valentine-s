eff1154762e4abf7a0a601784cc7b433fb3814d9
prasanna591 <prasanna30102005@gmail.com>
2025-02-03 18:39:30 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/your-repo-name/", // Important! Must match repository name
+  base: "/valentine-s/", // Use exact repository name
   plugins: [react()],
 });
